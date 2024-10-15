9ad5ebe7d50faea7a712448b9cad1992589f25cb
SamuelGracian <idv23c.ogracian@uartesdigitales.edu.mx>
2024-10-14 19:49:52 -0600

    Client
    Craeted a client, to connecto to the udp server.
    function kept alive, a function that keeps sending messages to the server, and not lose the conection.

--- TestClient/Source Files/Source.cpp
+++ TestClient/Source Files/Source.cpp
@@ -2,13 +2,58 @@
 #include <optional>
 
 #include <SFML/Network.hpp>
-
+#include <SFML/Graphics.hpp>
 #include "PackageHeader.h"
 
 using std::cout;
+using std::vector;
 using std::endl;
+using std::string;
 
 constexpr unsigned int gPort = 55001;
+
+void Keep_Alive(sf::UdpSocket& socket, PackageHeader header, sf::IpAddress ip )
+{
+	vector <char> Buffer(1024);
+	size_t				Recieved = 0;
+	std::optional < sf::IpAddress> Sender;
+	unsigned short		Sender_Port;
+
+	if (
+		socket.receive
+		(
+			Buffer.data(),
+			Buffer.size(),
+			Recieved,
+			Sender,
+			Sender_Port
+		)
+		==
+		sf::Socket::Status::Done
+		)
+	{
+		cout << "Message recieved from sender " << string(Buffer.begin(), Buffer.end() + Recieved) << std::endl;
+	}
+
+	sf::sleep(sf::seconds(5));
+	cout << "Im still alive :)" << endl;
+
+	if
+		(
+			socket.send
+			(
+				&header,
+				sizeof(PackageHeader),
+				ip,
+				gPort
+			)
+			!=
+			sf::Socket::Status::Done
+			)
+	{
+		cout << "Failed to be alive. :(" << endl;
+	}
+}
 
 void connectToUDPServer()
 {
@@ -36,7 +81,7 @@ void connectToUDPServer()
 			gPort
 		)
 		!=
-		sf::Socket::Status::Done
+		   sf::Socket::Status::Done
 		)
 	{
 		cout << "Failed to connect to the server" << endl;
@@ -44,6 +89,12 @@ void connectToUDPServer()
 	}
 
 	cout << "Connected to the server" << ip.value() << endl;
+
+	////////////// FUNCION NUEVA ////////////
+	while (true)
+	{
+		Keep_Alive(socket, header, *ip);
+	}
 }
 
 int main()
